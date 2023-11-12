ca27a40f7ad1e7b15d2c938cd5f2fc630242e988
Enes Aktaş <ensaktas0@gmail.com>
2023-11-12 14:52:19 +0300

    fix: react logo

--- components/Header.jsx
+++ components/Header.jsx
@@ -3,7 +3,7 @@ import React from "react";
 export default function Header() {
   return (
     <header className="header">
-      <img src="/logo512.png" alt="react icon" width={140} />
+      <img src="./logo512.png" alt="react icon" width={140} />
       <h1>THE REACT QUIZ</h1>
     </header>
   );
